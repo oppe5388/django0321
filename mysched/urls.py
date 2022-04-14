b38584b407edda95097d476aa73d5646fbf8a360
oppe5388 <oppe5388@yahoo.co.jp>
2022-04-14 22:24:11 +0900

    mychedタブ表示に変更

--- mysched/urls.py
+++ mysched/urls.py
@@ -5,6 +5,5 @@ app_name = 'mysched'
 
 urlpatterns = [
     path('', views.mysched, name='mysched'),
-    path('fixdisp', views.mysched_fixdisp, name='fixdisp'),
     # path('', views.information_list, name='index'),
 ]
