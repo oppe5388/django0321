bd8c277423586765515b5e2f09021e9b48003934
oppe5388 <oppe5388@yahoo.co.jp>
2022-05-07 17:39:03 +0900

    HEAD確認

--- mycontact/models.py
+++ mycontact/models.py
@@ -2,21 +2,6 @@ from django.db import models
 from django.urls import reverse
 import os
 
-class Contacts(models.Model):
-    incoming = models.CharField(max_length=20, verbose_name="相手")
-    name = models.TextField(max_length=100, verbose_name="窓口")
-    title = models.TextField(max_length=100, verbose_name="業務")
-    job = models.TextField(max_length=1000, verbose_name="詳細")
-    tel = models.TextField(max_length=100, verbose_name="TEL")
-    hours = models.TextField(max_length=100, verbose_name="時間")
-    searchwords = models.CharField(max_length=200, null=True, blank=True, verbose_name="検索ワード")
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = "問い合わせ先"
-
 
 class Dealers(models.Model):
     code5 = models.CharField(max_length=10, verbose_name="コード5桁")
@@ -54,3 +39,19 @@ class Shops(models.Model):
         
     class Meta:
         verbose_name_plural = "店舗"
+
+
+class Contacts(models.Model):
+    incoming = models.CharField(max_length=20, verbose_name="相手")
+    name = models.TextField(max_length=100, verbose_name="窓口")
+    title = models.TextField(max_length=100, verbose_name="業務")
+    job = models.TextField(max_length=1000, verbose_name="詳細")
+    tel = models.TextField(max_length=100, verbose_name="TEL")
+    hours = models.TextField(max_length=100, verbose_name="時間")
+    searchwords = models.CharField(max_length=200, null=True, blank=True, verbose_name="検索ワード")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "問い合わせ先"
