df503cbb409b7b51b171c6c3dcbd672e234d10ba
oppe5388 <oppe5388@yahoo.co.jp>
2022-06-08 00:46:14 +0900

    context_processors修正

--- myreport/context_processors.py
+++ myreport/context_processors.py
@@ -1,17 +1,17 @@
-# from .models import *
-# from accounts.models import User
-# from django.contrib.auth.decorators import login_required
+from .models import *
 
-# @login_required
-# def my_context_processor(request):
-#     pass
-#     # 未読件数を表示
-#     c_num = ReportRead.objects.filter(user=request.user).count()
-#     midoku = ""
-#     if c_num > 0:
-#         midoku = str(c_num)        
+def my_context_processor(request):
+    if request.user.id is None:
+        return {}
+    else:
+        # 未読件数を表示
+        c_num = ReportRead.objects.filter(user=request.user).count()
+        midoku = ""
+        if c_num > 0:
+            midoku = str(c_num)        
 
-#     return {
-#         'midoku': midoku,
-#         'site_name': 'Hogehoge Site',
-#     }
+        return {
+            'midoku': midoku,
+            'site_name': 'Hogehoge Site',
+        }
+    
