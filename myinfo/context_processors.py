bfbc0b24fa326700de20d8fa32329d03922e0c0f
oppe5388 <oppe5388@yahoo.co.jp>
2022-06-11 23:12:10 +0900

    おしらせ未読件数表示

--- myinfo/context_processors.py
+++ myinfo/context_processors.py
@@ -1,5 +1,21 @@
-def my_context_processor(req):
-    return {
-        'domain_name': 'https://hogehoge.com',
-        'site_name': 'Hogehoge Site',
-    }
+# def my_context_processor(req):
+#     return {
+#         'domain_name': 'https://hogehoge.com',
+#         'site_name': 'Hogehoge Site',
+#     }
+
+from .models import *
+
+def my_context_processor(request):
+    if request.user.id is None:
+        return {}
+    else:
+        # 未読件数を表示
+        c_num = ReadStates.objects.filter(user=request.user, information__is_draft=False).count()
+        midoku_info = ""
+        if c_num > 0:
+            midoku_info = str(c_num)        
+
+        return {
+            'midoku_info': midoku_info,
+        }
