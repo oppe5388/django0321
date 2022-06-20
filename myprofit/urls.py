cc55ab9ef42ade5f065f495f88130a0e7d19720b
oppe5388 <oppe5388@yahoo.co.jp>
2022-06-20 18:47:54 +0900

    new02まで

--- myprofit/urls.py
+++ myprofit/urls.py
@@ -7,5 +7,8 @@ app_name = 'myprofit'
 
 urlpatterns = [
     path('', views.profit_top, name='profit_top'),
+    path('new01', views.new01, name='new01'),
+    path('new02', views.new02, name='new02'),
+    path('new03', views.new02, name='new03'),
 
 ]
--- myprofit/views.py
+++ myprofit/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 
 
 def profit_top(request):
-    context ={
-            'aaa': '',
-        }
-
+    context ={'aaa': ''}
     return render(request, 'myprofit/top.html', context)
+
+def new01(request):
+    context ={'aaa': ''}
+    return render(request, 'myprofit/new01.html', context)
+
+def new02(request):
+    context ={'aaa': ''}
+    return render(request, 'myprofit/new02.html', context)
+
+def new03(request):
+    context ={'aaa': ''}
+    return render(request, 'myprofit/new03.html', context)
+    
