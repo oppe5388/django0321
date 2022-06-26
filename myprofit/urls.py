1e04311bf4e928eb5a17671989c7b24d2f94e44e
oppe5388 <oppe5388@yahoo.co.jp>
2022-06-26 19:26:36 +0900

    途中

--- myprofit/urls.py
+++ myprofit/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('new02', views.new02, name='new02'),
     path('new03', views.new03, name='new03'),
     path('new04', views.new04, name='new04'),
+    path('new02_02', views.new02_02, name='new02_02'),
+    path('new03_02', views.new03_02, name='new03_02'),
 
 ]
--- myprofit/views.py
+++ myprofit/views.py
@@ -6,18 +6,19 @@ def profit_top(request):
     return render(request, 'myprofit/top.html', context)
 
 def new01(request):
-    context ={'aaa': ''}
-    return render(request, 'myprofit/new01.html', context)
+    return render(request, 'myprofit/new01.html')
 
 def new02(request):
-    context ={'aaa': ''}
-    return render(request, 'myprofit/new02.html', context)
+    return render(request, 'myprofit/new02.html')
 
 def new03(request):
-    context ={'aaa': ''}
-    return render(request, 'myprofit/new03.html', context)
+    return render(request, 'myprofit/new03.html')
 
 def new04(request):
-    context ={'aaa': ''}
-    return render(request, 'myprofit/new04.html', context)
-    
+    return render(request, 'myprofit/new04.html')
+
+def new02_02(request):
+    return render(request, 'myprofit/new02_02.html')
+
+def new03_02(request):
+    return render(request, 'myprofit/new03_02.html')
