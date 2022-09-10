ffc0153e657a32194cee73e37057575813f2dbe8
oppe5388 <oppe5388@yahoo.co.jp>
2022-09-10 15:34:14 +0900

    CA追加をbulk_createに変更

--- myinfo/management/commands/mycommand.py
+++ myinfo/management/commands/mycommand.py
@@ -13,10 +13,24 @@ class Command(BaseCommand):
         #シートの最終行を取得
         Sheet_Max_Row = ws.max_row
 
+        add_cas = []  # bulk_create用　作成クエリの配列
+
         # 行をループ
         for j in range(2,Sheet_Max_Row + 1):#2行目から
-            #レコード作成
-            CAs.objects.create(
+            # #1件ずつのレコード作成
+            # CAs.objects.create(
+            #     id = int(ws.cell(row=j, column=1).value),#全データ削除してあればid指定してOK
+            #     dealer = Dealers.objects.get(pk=int(ws.cell(row=j, column=2).value)),#ForeignKeyフィールド
+            #     shop = ws.cell(row=j, column=3).value,
+            #     shopcode = ws.cell(row=j, column=4).value,
+            #     cacode = ws.cell(row=j, column=5).value,
+            #     name = ws.cell(row=j, column=6).value,
+            #     kana = ws.cell(row=j, column=7).value,
+            # )
+            # print(j)
+
+            # bulk_createバージョン
+            ca = CAs(
                 id = int(ws.cell(row=j, column=1).value),#全データ削除してあればid指定してOK
                 dealer = Dealers.objects.get(pk=int(ws.cell(row=j, column=2).value)),#ForeignKeyフィールド
                 shop = ws.cell(row=j, column=3).value,
@@ -25,7 +39,10 @@ class Command(BaseCommand):
                 name = ws.cell(row=j, column=6).value,
                 kana = ws.cell(row=j, column=7).value,
             )
+            add_cas.append(ca)  # 作成クエリを配列に格納
             print(j)
+
+        CAs.objects.bulk_create(add_cas)  # ここで追加のクエリ1つ発行
 
     # 引数を使う場合は必要
     def add_arguments(self, parser):
