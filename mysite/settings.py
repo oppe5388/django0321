e8dc17bd529169cecdf80bf44707134ea889fb71
oppe5388 <oppe5388@yahoo.co.jp>
2022-10-16 18:11:58 +0900

    キャッシュ対策にSTATICFILES_STORAGE追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -170,6 +170,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # #bs4sampleで追加
 # STATICFILES_DIRS =(
 #     os.path.join(BASE_DIR,'static'),
