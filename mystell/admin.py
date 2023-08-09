834b2ee83250ac5a6a8ec039544ababb36dfd5bc
oppe5388 <oppe5388@yahoo.co.jp>
2023-08-09 20:47:53 +0900

    stに画像フィールド追加

--- mystell/admin.py
+++ mystell/admin.py
@@ -7,6 +7,9 @@ from import_export.formats import base_formats
 
 from django.utils.html import strip_tags
 
+from django.utils.html import format_html
+from django.utils import timezone
+
 class TaskResource(ModelResource):
     # field名とcsvの列名が異なる場合はここで指定する。
 
@@ -17,14 +20,25 @@ class TaskResource(ModelResource):
 
 #インポート、エクスポート
 class TaskAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    list_display = ('id', 'created_at', 'request_content', 'is_completed', 'response_content', 'release_date')
+    list_display = ('display_image', 'get_created_at_format', 'is_completed', 'release_date', 'request_content', 'response_content', )
     # list_display = ('id', 'created_at', 'short_request_content', 'is_completed', 'short_response_content', 'release_date')
     resource_class = TaskResource
     list_filter = ('is_completed', 'release_date')
     search_fields = ('request_content', 'response_content', 'notes')
     ordering = ('id',)
-    list_editable = ('created_at', 'request_content', 'is_completed', 'response_content', 'release_date') # 追記箇所
+    list_editable = ('request_content', 'is_completed', 'release_date', 'response_content') # 追記箇所
+
+    # created_atフィールドのカスタム表示メソッド
+    def get_created_at_format(self, obj):
+        return obj.created_at.strftime('%m/%d(%a)') if obj.created_at else None
+    get_created_at_format.short_description = '発生日'
     
+    # 画像のプレビューを表示するカスタムメソッド
+    def display_image(self, obj):
+        if obj.image:
+            return format_html('<a href="{}" target="_blank"><img src="{}" style="max-height:100px;max-width:60px;" /></a>', obj.image.url, obj.image.url)
+        return "No Image"
+    display_image.short_description = 'Image'
     
     # 未使用
     def short_request_content(self, obj):
@@ -37,7 +51,4 @@ class TaskAdmin(ImportExportModelAdmin, admin.ModelAdmin):
         return no_html[:50]
     short_response_content.short_description = 'Short Response Content'
 
-
 admin.site.register(Task, TaskAdmin)
-    
-    
--- mystell/models.py
+++ mystell/models.py
@@ -7,19 +7,22 @@ from tinymce import models as tinymce_models
 
 class Task(models.Model):
     # created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.DateField(null=True, blank=True)
+    created_at = models.DateField(null=True, blank=True, verbose_name="発生日")
     
-    request_content = models.TextField(null=True, blank=True)
+    request_content = models.TextField(null=True, blank=True, verbose_name="内容")
     # request_content = tinymce_models.HTMLField(null=True, blank=True)
     
-    is_completed = models.BooleanField(default=False) #help_text='完了'
-    release_date = models.DateField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False, verbose_name="完了") #help_text='完了'
+    release_date = models.DateField(null=True, blank=True, verbose_name="リリース日")
     
-    response_content = models.TextField(null=True, blank=True)
+    response_content = models.TextField(null=True, blank=True, verbose_name="回答")
     # response_content = tinymce_models.HTMLField(null=True, blank=True)
     
     notes = models.TextField(null=True, blank=True)
     # notes = tinymce_models.HTMLField(null=True, blank=True)
+    
+    # 画像を格納するためのフィールド
+    image = models.ImageField(upload_to = 'uploads/st/', null=True, blank=True, verbose_name='画像')
     
     class Meta:
         verbose_name_plural = "st課題管理"
